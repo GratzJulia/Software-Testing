a3c43a84595059fa933b36a1ad081cc078c62911
GratzJulia <julia.gratz@aluno.cefet-rj.br>
2020-06-13 19:38:04 -0300

    first

---
+++ TestesUnitarios/src/main/java/code/Usuario.java
@@ -0,0 +1,20 @@
+package code;
+
+public class Usuario {
+
+	private String nome;
+	
+	public Usuario() {}
+	
+	public Usuario(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
